68c842de335a40d26712dbf16b0278855188213d
Justin <jlowell.carpenter@gmail.com>
2020-03-09 11:52:23 -0500

    Added wandering, animals
    Added bunnies and chickens
    Animals now wander randomly when they have nothing better to do

--- Assets/_Scripts/Movement.cs
+++ Assets/_Scripts/Movement.cs
@@ -4,35 +4,74 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody2D rb;
 
     public float movementSpeed = 1.0f;//how fast the animal moves by default
     public float walkMultiplier = 1.0f;
     public float waterMultiplier = .5f;
     public float sprintMultiplier = 2.0f;
+    public float wanderMultiplier = .5f;
     bool isSprinting = false;
     public int inWater = 0;//any value over 0 is in water
     float totalMoveSpeed = 1.0f;
 
+    const float MOVEMENT_NORMALIZATION = 1f;//adjusts the overall speed of movement to accomodate the size of objects relative to the world
+
+    bool isPlayerControlled = false;
+
     public float environmentalSpeedReduction = 1.0f;
     public List<float> speedModifiers = new List<float>();
+
+    float wanderTime = 0f;
+    Vector2 wanderDirection;
+    bool isPerformingAction = false;
+    bool isWandering = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         CalculateSpeed();
+        WanderRandomly();
         InputMove();
+    }
+
+    private void FixedUpdate()
+    {
+        if(isWandering)
+        {
+            //rb.AddForce(wanderDirection * Time.deltaTime * movementSpeed * totalMoveSpeed * wanderMultiplier);
+            transform.Translate((wanderDirection * Time.deltaTime * movementSpeed * totalMoveSpeed * wanderMultiplier));
+            wanderTime -= Time.deltaTime;
+            if(wanderTime <= 0) { isWandering = false; }
+        }
+        if(rb.velocity.magnitude > totalMoveSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * totalMoveSpeed;
+        }
+    }
+
+    void WanderRandomly()
+    {
+        if(isPlayerControlled || isWandering || isPerformingAction)
+        {
+            return;
+        }
+        wanderDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        wanderTime = Random.Range(1f, 5f);
+        isWandering = true;
     }
 
     #region Manual Movement
 
     void CalculateSpeed()
     {
-        totalMoveSpeed = 1;
+        totalMoveSpeed = 1 * MOVEMENT_NORMALIZATION;
 
         if (isSprinting) totalMoveSpeed *= sprintMultiplier;
         else totalMoveSpeed *= walkMultiplier;
@@ -50,27 +89,29 @@ public class Movement : MonoBehaviour
 
     void InputSprint()
     {
+        if(!isPlayerControlled) { return; }
         if (Input.GetKey(KeyCode.LeftShift)) isSprinting = true;
         else isSprinting = false;
     }
 
     void InputMove()
     {
-        if(Input.GetKey("w"))
+        if (!isPlayerControlled) { return; }
+        if (Input.GetKey("w"))
         {
-            transform.Translate(Vector3.up * Time.deltaTime * movementSpeed * totalMoveSpeed);
+            rb.AddForce(Vector3.up * Time.deltaTime * movementSpeed * totalMoveSpeed);
         }
         if (Input.GetKey("s"))
         {
-            transform.Translate(Vector3.down * Time.deltaTime * movementSpeed * totalMoveSpeed);
+            rb.AddForce(Vector3.down * Time.deltaTime * movementSpeed * totalMoveSpeed);
         }
         if (Input.GetKey("a"))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * movementSpeed * totalMoveSpeed);
+            rb.AddForce(Vector3.left * Time.deltaTime * movementSpeed * totalMoveSpeed);
         }
         if (Input.GetKey("d"))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * movementSpeed * totalMoveSpeed);
+            rb.AddForce(Vector3.right * Time.deltaTime * movementSpeed * totalMoveSpeed);
         }
     }
 
